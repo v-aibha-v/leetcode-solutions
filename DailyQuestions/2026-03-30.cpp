3eaf56f5dc72f4924fee267da6c97cf179e9cd9a
v-aibha-v <vaibhavv08.05@gmail.com>
2026-03-30 11:27:55 +0530

    Updated 2026-03-30.cpp

--- DailyQuestions/2026-03-30.cpp
+++ DailyQuestions/2026-03-30.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        //freq diff for even and odd idx
+        vector<int> even(26, 0), odd(26, 0);
+        int n = s1.length();
+        for (int i = 0; i < n; ++i) {
+            //update freq
+            if (i % 2 == 0) {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+        //if all freq =0 -> strings are equivalent
+        for (int i = 0; i < 26; ++i) {
+            if (even[i] != 0 or odd[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
