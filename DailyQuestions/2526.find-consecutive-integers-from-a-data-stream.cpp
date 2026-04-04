66f9605d22ae666af0ca7642a046c25ea1feb702
v-aibha-v <vaibhavv08.05@gmail.com>
2026-04-04 14:07:49 +0530

    Add files via upload
    2075. Decode the Slanted Ciphertext

---
+++ DailyQuestions/2526.find-consecutive-integers-from-a-data-stream.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=2526 lang=cpp
+ *
+ * [2526] Find Consecutive Integers from a Data Stream
+ */
+
+// @lc code=start
+class DataStream {
+public:
+    DataStream(int value, int k) {
+        
+    }
+    
+    bool consec(int num) {
+        
+    }
+};
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream* obj = new DataStream(value, k);
+ * bool param_1 = obj->consec(num);
+ */
+// @lc code=end
+
